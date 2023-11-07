5bf922fd4c401b9679926159c4515e7d0b65d184
leekj601 <leekj601@naver.com>
2023-11-07 16:27:49 +0900

    업로드 완료

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 const port = 3000;
+const upload = multer({ dest: 'uploads/' });
+
 
 // 'public' 폴더를 정적 파일로 서빙하는 데 사용
 app.use(express.static('public'));
-
+app.post('/upload', upload.single('file'), function (req, res) {
+  // 파일 처리 로직
+  console.log(req.file); // 업로드된 파일 정보
+  // ... Firebase에 파일 업로드 로직을 추가
+  res.send('File uploaded successfully.');
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
