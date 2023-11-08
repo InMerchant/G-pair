33d80a719d91f1f30fa2a77a732a4fbd143376f3
kangwongyu795 <ee4928@naver.com>
2023-11-08 18:40:59 +0900

    로그아웃 구현

---
+++ public/js/login/log out.js
@@ -0,0 +1,14 @@
+
+import { signOut } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { auth } from "../firebase.js"; 
+const logoutButton = document.getElementById('logout-button'); // 로그아웃 버튼의 ID로 대체하세요.
+logoutButton.addEventListener('click', () => {
+  signOut(auth).then(() => {
+    // 로그아웃 성공 시 처리
+    console.log('User signed out.');
+    window.location.href = 'login.html';
+  }).catch((error) => {
+    // 로그아웃 실패 시 처리
+    console.error('Sign out error', error);
+  });
+});
