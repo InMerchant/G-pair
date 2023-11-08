d4d1a59f4774db991eff3aa950f8ba5a2365a1c6
leekj601 <leekj601@naver.com>
2023-11-08 17:25:01 +0900

    오류 수정

--- public/js/homepageload.js
+++ public/js/homepageload.js
@@ -58,7 +58,7 @@ const createCard = (data) => {
                 <!-- Product actions-->
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                     <div class="text-center">
-                        <a class="btn btn-outline-dark mt-auto" href="${data.detailUrl}">접속</a>
+                        <a class="btn btn-outline-dark mt-auto" href="/detail?name=${data.webtoonID}">접속</a>
                     </div>
                 </div>
             </div>
