3071b8e09b48ed629125f84fdbb10d159a1fb127
InMerchant <106319540+InMerchant@users.noreply.github.com>
2023-11-10 14:46:29 +0900

    에피소드 페이지 추가 메모장 폴더 추가

--- public/js/upload,loading/detail_HTML_tableLoad.js
+++ public/js/upload,loading/detail_HTML_tableLoad.js
@@ -19,14 +19,15 @@ export async function loadEpisodesToTable(webtoonID) {
     
             episodesSnapshot.forEach((episodeDoc) => {
             const episode = episodeDoc.data();
+            const formattedUploadDate = formatDate(episode.uploadDate);
             const row = `
                 <tr>
                 <td>${episode.episodeID}화</td>
+                <td>${episode.subTitle}</td>
                 <td>${episode.test}</td>
-                <td>${episode.test}</td>
-                <td>${episode.test}</td>
-                <td>${episode.test}</td>
-                <td>${episode.test}</td>
+                <td>${episode.imgSearchCount}</td>
+                <td>${episode.recommend}</td>
+                <td>${formattedUploadDate}</td>
                 </tr>
             `;
             tableBody.innerHTML += row;
@@ -35,6 +36,16 @@ export async function loadEpisodesToTable(webtoonID) {
     } catch (error) {
       console.error("Error loading episodes: ", error);
     }
+}
+
+// Timestamp 객체에서 yy-mm-dd 형식의 날짜 문자열을 생성하는 함수
+function formatDate(timestamp) {
+    const date = new Date(timestamp.seconds * 1000); // Firestore의 Timestamp에서 JavaScript의 Date 객체 생성
+    const year = date.getFullYear().toString().substr(-2); // 연도의 뒤 두 자리
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월 (getMonth()는 0부터 시작하므로 1을 더합니다)
+    const day = date.getDate().toString().padStart(2, '0'); // 일
+
+    return `${year}-${month}-${day}`; // 포맷에 맞게 반환
 }
 
 export async function loadDataAndInitializeTable(webtoonID) {
@@ -47,21 +58,20 @@ export async function loadDataAndInitializeTable(webtoonID) {
         
         // 'dataTable.init' 이벤트가 발생하면 실행되는 콜백 함수 내부에서 'dataTable' 변수를 사용합니다.
         dataTable.on('datatable.init', () => {
-            addRowClickEvent();
+            addRowClickEvent(webtoonID);
         });
     }
 }
 
 // 이 함수는 각 테이블 행에 클릭 이벤트 리스너를 추가합니다.
-function addRowClickEvent() {
+function addRowClickEvent(webtoonID) {
     const rows = document.querySelectorAll('#datatablesSimple tbody tr');
     rows.forEach((row) => {
         row.addEventListener('click', function() {
-            const rowIndex = this.dataset.index; // 'data-index' 값을 가져옵니다.
-            console.log(`Row with data-index ${rowIndex} clicked`);
             const episodeId = this.cells[0].textContent;
-            console.log(`Clicked row episodeID: ${episodeId}`);
-            // 여기에서 rowIndex를 기반으로 필요한 기능을 구현합니다.
+
+            //각 에피소드로 이동하는 기능
+            window.location.href = `episode.html?webtoonID=${webtoonID}&id=${episodeId}`;
         });
     });
 }
--- public/js/upload,loading/episodeLoad.js
+++ public/js/upload,loading/episodeLoad.js
@@ -1,46 +1,18 @@
-import { db } from '../firebase.js';
-import { collection, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js';
+//import { webtoonIDInfo } from './detail_HTML_tableLoad.js'
+//import { getEpisodeData } from './episodeSearch.js'
 
-// 특정 웹툰 ID와 에피소드 ID에 해당하는 에피소드 데이터를 가져오는 함수
-export async function getEpisodeData(webtoonID, episodeID) {
-    try {
-      // 'webtoonDATA' 컬렉션에 대한 참조
-      const webtoonCollectionRef = collection(db, "webtoonDATA");
-  
-      // 'webtoonID' 필드가 특정 webtoonID 값을 가진 문서를 찾는 쿼리
-      const webtoonQuery = query(webtoonCollectionRef, where("webtoonID", "==", webtoonID));
-  
-      // 쿼리 실행
-      const webtoonQuerySnapshot = await getDocs(webtoonQuery);
-  
-      // 문서가 존재하는 경우
-      if (!webtoonQuerySnapshot.empty) {
-        // 해당 문서의 'Episode' 서브컬렉션에 대한 참조를 가져옴
-        const webtoonDocRef = webtoonQuerySnapshot.docs[0].ref;
-        const episodeCollectionRef = collection(webtoonDocRef, "Episode");
-  
-        // 'episodeID' 필드가 특정 episodeID 값을 가진 문서를 찾는 쿼리
-        const episodeQuery = query(episodeCollectionRef, where("episodeID", "==", episodeID));
-  
-        // 쿼리 실행
-        const episodeQuerySnapshot = await getDocs(episodeQuery);
-  
-        // 에피소드 문서가 존재하는 경우
-        if (!episodeQuerySnapshot.empty) {
-          // 에피소드 데이터 반환
-          const episodeData = episodeQuerySnapshot.docs[0].data();
-          const documentID = episodeQuerySnapshot.docs[0].id;       //찾은 에피소드 화수 ex) 1화
-          return episodeData;
-        } else {
-          console.log(`No episode matching episodeID ${episodeID}`);
-          return null;
-        }
-      } else {
-        console.log(`No document matching webtoonID ${webtoonID}`);
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching episode data: ", error);
-      return null;
-    }
-  }
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const episodeStr = urlParams.get("id");
+const webtoonID = urlParams.get("webtoonID");
+
+function extractNumberFromString(str) {
+    const matches = str.match(/\d+/); // 숫자가 하나 이상 연속되는 부분을 찾기
+    return matches ? parseInt(matches[0], 10) : null; // 찾은 숫자를 정수로 변환
+}
+const episodeID = extractNumberFromString(episodeStr); // 정수로 변환된 값 저장
+
+console.log(episodeID);
+console.log(webtoonID)
+
+
