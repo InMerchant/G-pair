1e89e3257d408490b93a77598bd046a1a1f588b7
kangwongyu795 <ee4928@naver.com>
2023-11-13 11:11:40 +0900

    장면 검색 상황 검색 일부 구현

--- public/js/Search history/Search.js
+++ public/js/Search history/Search.js
@@ -6,19 +6,42 @@ const index = client.initIndex('webtoonDATASEARCH');
 
 // 검색 버튼과 입력 필드를 가져옵니다.
 const searchButton = document.querySelector('.btn-primary');
-const searchInput = document.getElementById('email'); // 이메일 입력란을 검색 입력란으로 사용
+const searchInput = document.getElementById('email'); // 검색 입력란 ID 확인 필요
 const resultsContainer = document.querySelector('.card-body');
 
 // 검색 버튼에 이벤트 리스너를 추가합니다.
 searchButton.addEventListener('click', () => {
     const query = searchInput.value;
+
     index.search(query).then(({ hits }) => {
-        // 검색 결과 처리
-        resultsContainer.innerHTML = hits.map(hit => 
-            `<div><strong>${hit.title}</strong>
-                <p>Author: ${hit.anthor}</p>
-        </div>`// Algolia 인덱스의 필드에 따라 변경
-        ).join('');
+        if (hits.length === 0) {
+            // 검색 결과가 없을 경우
+            resultsContainer.innerHTML = '<div>검색 결과가 없습니다.</div>';
+        } else {
+            // 검색 결과 처리
+            resultsContainer.innerHTML = hits.map(hit => {
+                let matchedContent = '';
+
+                // 각 화별로 검색 쿼리가 포함된 대사와 상황만 찾기
+                for (let key in hit) {
+                    if (key.includes('대사') || key.includes('상황')) {
+                        const episodeNumber = key.split('화')[0];
+                        const relevantLines = hit[key].filter(line => line.includes(query));
+
+                        if (relevantLines.length > 0) {
+                            matchedContent += `<div><strong>${hit.title}</strong> - ${episodeNumber}화 ${key.includes('대사') ? '대사' : '상황'}: ${relevantLines.join(', ')}</div>`;
+                        }
+                    }
+                }
+
+                return matchedContent;
+            }).join('');
+
+            if (!resultsContainer.innerHTML) {
+                // 검색어가 포함된 대사나 상황이 없는 경우
+                resultsContainer.innerHTML = '<div>검색 결과가 없습니다.</div>';
+            }
+        }
     }).catch(err => {
         console.error(err);
         resultsContainer.innerHTML = '<div>검색 중 오류가 발생했습니다.</div>';
