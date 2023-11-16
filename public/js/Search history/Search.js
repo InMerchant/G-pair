f5cd1c7372da75ba0b6f38a45715f3ca86d3bf67
kangwongyu795 <ee4928@naver.com>
2023-11-16 15:05:54 +0900

    검색 업데이트

--- public/js/Search history/Search.js
+++ public/js/Search history/Search.js
@@ -1,25 +1,31 @@
 // Algolia 클라이언트 설정
 import { collection, query, where, getDocs, updateDoc, doc, increment } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
-import { db } from '../firebase.js'; // 경로는 실제 설정에 맞게 조정해야 합니다.
+import { db } from '../firebase.js'; 
 
 
 const client = algoliasearch('TOBZK90LFP', '7e6fca838d4c8a9dc0bee205f5a7a380');
 const index = client.initIndex('webtoonDATASEARCH');
 
-// 검색 버튼과 입력 필드를 가져옵니다.
 const searchButton = document.querySelector('.btn-primary');
 const searchInput = document.getElementById('searchQuery');
 const searchTypeSelect = document.getElementById('searchType');
+const webtoonNameSelect = document.getElementById('WebtoonID'); // 웹툰 이름 선택 필드
 const resultsContainer = document.querySelector('.card-body');
+
 // 검색 버튼에 이벤트 리스너를 추가합니다.
 searchButton.addEventListener('click', () => {
     const query = searchInput.value;
     const searchType = searchTypeSelect.value;
+    const selectedWebtoon = webtoonNameSelect.value; // 선택된 웹툰 이름
 
     index.search(query).then(({ hits }) => {
         let searchResults = '';
 
         hits.forEach(hit => {
+            if (selectedWebtoon !== 'all' && hit.title !== selectedWebtoon) {
+                return; // 선택된 웹툰 이름과 일치하지 않으면 생략
+            }
+
             for (let key in hit) {
                 if (searchType === 'dialogue' && key.includes('대사') && hit[key].some(line => line.includes(query))) {
                     const episodeNumber = key.split('화')[0];
@@ -43,18 +49,26 @@ searchButton.addEventListener('click', () => {
     });
 });
 
-// 검색 결과 항목 생성 함수
 function createSearchResultItem(hit, episodeNumber, key, type, query) {
-    const lines = Array.isArray(hit[key]) ? hit[key] : [];
-    const filteredLines = lines.filter(line => line.includes(query));
-    const content = filteredLines.map(line => `<div>${line}</div>`).join('');
+    const dialogueOrSituationLines = Array.isArray(hit[key]) ? hit[key] : [];
+    const episodeImageUrlKey = episodeNumber + '화 url';
+    const episodeImageUrls = hit[episodeImageUrlKey] || [];
+    const defaultImageUrl = `https://firebasestorage.googleapis.com/v0/b/look-b1624.appspot.com/o/${encodeURIComponent(hit.webtoonID)}%2Fsign.png?alt=media`;
 
-    // 이미지 URL 구성
-    const imageUrl = `https://firebasestorage.googleapis.com/v0/b/look-b1624.appspot.com/o/${encodeURIComponent(hit.webtoonID)}%2Fsign.png?alt=media`;
+    const content = dialogueOrSituationLines.map((line, index) => {
+        // 해당 순서의 이미지 URL 가져오기
+        const imageUrl = episodeImageUrls.length > index ? episodeImageUrls[index] : defaultImageUrl;
+        const imageElement = line.includes(query) ? `<img src="${imageUrl}" class="search-result-image" alt="Episode Image">` : '';
+        
+        return imageElement ? `
+            <div class="search-result-content">
+                ${imageElement}
+                <div>${line}</div>
+            </div>` : '';
+    }).join('');
 
     return `
     <div class="card search-result-item mb-3" data-webtoon-id="${hit.webtoonID}" data-episode-number="${episodeNumber}" data-search-type="${type}">
-        <img src="${imageUrl}" class="card-img-top" alt="${hit.title}" style="height: 200px; object-fit: contain; width: 100%;">
         <div class="card-body">
             <h5 class="card-title">${hit.title}</h5>
             <p class="card-text">작가: ${hit.author}</p>
@@ -62,6 +76,8 @@ function createSearchResultItem(hit, episodeNumber, key, type, query) {
         </div>
     </div>`;
 }
+
+
 
 
 
@@ -118,3 +134,23 @@ function addClickEventToSearchResults() {
         });
     });
 }
+
+
+// Firestore에서 Search 컬렉션의 데이터를 읽어와 드롭다운 초기화
+async function initializeWebtoonDropdown() {
+    const searchRef = collection(db, "Search"); // 'Search' 컬렉션 참조
+    const querySnapshot = await getDocs(searchRef); // 데이터 읽기
+    const webtoonDropdown = document.getElementById('WebtoonID'); // 드롭다운 선택
+
+    // Firestore에서 읽어온 문서의 title 필드를 바탕으로 드롭다운 옵션 생성
+    querySnapshot.forEach((doc) => {
+        const webtoonTitle = doc.data().title; // title 필드
+        const option = document.createElement('option');
+        option.value = webtoonTitle;
+        option.textContent = webtoonTitle;
+        webtoonDropdown.appendChild(option); // 드롭다운에 옵션 추가
+    });
+}
+
+// 페이지 로드 시 드롭다운 초기화 실행
+document.addEventListener('DOMContentLoaded', initializeWebtoonDropdown);
