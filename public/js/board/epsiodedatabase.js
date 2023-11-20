3f79af45b40aff601a0b5bf121c59f0e1acd9414
leekj601 <leekj601@naver.com>
2023-11-20 10:34:33 +0900

    에피소드 업로드(완)

--- public/js/board/epsiodedatabase.js
+++ public/js/board/epsiodedatabase.js
@@ -1,20 +1,26 @@
 import { db } from '../firebase.js';
 import { collection, getDocs, query, where,doc,setDoc} from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js';
 
-export async function updateOrCreateEpisode(webtoonID, episodeData, episodeNumber,filesData) {
+export async function updateOrCreateEpisode(webtoonID, episodeData, episodeNumber,filesData,response) {
   try {
     const webtoonCollectionRef = collection(db, "webtoonDATA");
     const q = query(webtoonCollectionRef, where("webtoonID", "==", webtoonID));
     const querySnapshot = await getDocs(q);
+    console.log(response)
     if (!querySnapshot.empty) {
       const webtoonDocRef = querySnapshot.docs[0].ref;
       const episodeDocRef = doc(collection(webtoonDocRef, "Episode"), episodeNumber);
       await setDoc(episodeDocRef, episodeData,);
       filesData.forEach(async (fileData, index) => {
+        const jsonData = response.find(item => item['파일 이름'] === fileData.name);
         const imageDocRef = doc(collection(episodeDocRef, "Image"), `image${index + 1}`);
         await setDoc(imageDocRef, {
-          url: fileData.url,   // 파일 URL
-          imgID: index + 1     // 정수 ID로 사용 (1부터 시작)
+          url: fileData.url, 
+          imgID: index + 1,  
+          상황: jsonData['문장1'], // JSON 파일에서 상황 가져오기
+          문장: jsonData['문장2'], // JSON 파일에서 문장 가져오기
+          상황라벨링: jsonData['문장1의 분류 라벨링'], // JSON 파일에서 상황 라벨링 가져오기
+          문장라벨링: jsonData['문장2의 분류 라벨링'] // JSON 파일에서 문장 라벨링 가져오기 
         });
       });
     } else {
