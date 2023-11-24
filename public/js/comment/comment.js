85cd09b851fe500081b88a59e652f9bb8965ff35
leekj601 <leekj601@naver.com>
2023-11-24 09:42:33 +0900

    댓글 수정중

--- public/js/comment/comment.js
+++ public/js/comment/comment.js
@@ -1,5 +1,5 @@
 import { db } from '../firebase.js';
-import { doc, setDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+import { doc, setDoc, arrayUnion,getDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 export function commentSave(webtoonID, episodeNumber, UID, comment) {
     if (comment) {
@@ -17,4 +17,33 @@ export function commentSave(webtoonID, episodeNumber, UID, comment) {
             console.error("Error adding comment: ", error);
         });
     }
+}
+
+export async function commentLoad(webtoonID,episodeNumber){
+    const docRef=doc(db,"COMMENT",webtoonID,"Episodes",episodeNumber);
+    try {
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data().Comments);
+            displayComments(docSnap.data().Comments);
+        } else {
+            console.log("No such document!");
+        }
+    } catch (error) {
+        console.error("Error getting document:", error);
+    }
+}
+
+function displayComments(comments) {
+    const commentsDiv = document.getElementById('comments');
+    commentsDiv.innerHTML = ''; // 기존 댓글을 지우고 새로 시작합니다.
+
+    // 각 댓글을 순회하며 웹 페이지에 추가합니다.
+    comments.forEach(comment => {
+        const p = document.createElement('p');
+        p.textContent = comment.comment; // 'text' 속성을 사용하여 실제 댓글 텍스트를 표시
+        commentsDiv.appendChild(p);
+    });
+      
 }
