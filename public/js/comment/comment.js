e31299c68a779cdb5449434fdb242929a6f8b27a
leekj601 <leekj601@naver.com>
2023-11-24 10:59:01 +0900

    댓글 기능 push

--- public/js/comment/comment.js
+++ public/js/comment/comment.js
@@ -1,7 +1,7 @@
 import { db } from '../firebase.js';
 import { getEpisodeDocData, getEpisodeData } from "../search_collection/episodeSearch.js"
-import { doc, arrayUnion,getDoc,updateDoc,increment,setDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
-
+import { doc, arrayUnion,getDoc,updateDoc,increment,setDoc,arrayRemove } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+//저장 함수
 export function commentSave(webtoonID, episodeNumber, UID, comment,episodeID) {
     if (comment) {
         const commentData = { userUid: UID, comment: comment };
@@ -11,42 +11,63 @@ export function commentSave(webtoonID, episodeNumber, UID, comment,episodeID) {
         setDoc(episodeRef, {
             Comments: arrayUnion(commentData)
         }, { merge: true })
-        commnetCount(webtoonID,episodeID)
+        commentCount(webtoonID,episodeID)
         .then(() => {
             console.log("Comment added successfully");
-            commentLoad(webtoonID, episodeNumber);
+            commentLoad(webtoonID, episodeNumber,episodeID,UID);
         })
         .catch((error) => {
             console.error("Error adding comment: ", error);
         });
     }
 }
-
-export async function commentLoad(webtoonID,episodeNumber){
+//로드 함수
+export async function commentLoad(webtoonID,episodeNumber,episodeID,UID){
     const episodeRef = doc(db, "COMMENT", webtoonID, "Episodes", episodeNumber);
     getDoc(episodeRef)
     .then((docSnap) => {
         if (docSnap.exists()) {
-            displayComments(docSnap.data().Comments);
+            displayComments(docSnap.data().Comments,webtoonID,episodeNumber,episodeID,UID);
         }
     })
     .catch((error) => {
         console.error("Error fetching comments: ", error);
     });
 }
-
-function displayComments(comments) {
+//댓글 출력
+function displayComments(comments, webtoonID, episodeNumber,episodeID,UID) {
     const commentsDiv = document.getElementById('comments');
     commentsDiv.innerHTML = ''; // 기존 댓글을 지우고 새로 시작합니다.
 
-    comments.forEach(comment => {
-      const p = document.createElement('p');
-      p.textContent = comment.comment; // 예시: comment 객체의 'comment' 속성 사용
-      commentsDiv.appendChild(p);
+    comments.forEach((comment) => {
+        // 댓글을 담을 요소 생성
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment-item');
+        const commentText = document.createElement('span');
+        commentText.classList.add('comment-text');
+        commentText.textContent = comment.comment;
+        commentElement.appendChild(commentText);
+
+        // 삭제 버튼을 담을 요소 생성
+        if (comment.userUid === UID) {
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-button');
+            deleteButton.textContent = '삭제';
+            deleteButton.addEventListener('click', function () {
+                console.log("Deleting comment:", comment);
+                deleteComment(webtoonID, episodeNumber, comment, episodeID,UID);
+            });
+
+            commentElement.appendChild(deleteButton);
+        }
+
+        // 댓글 요소를 댓글 목록에 추가
+        commentsDiv.appendChild(commentElement);
     });
 }
 
-async function commnetCount(webtoonID,episodeID){
+//카운트
+async function commentCount(webtoonID,episodeID){
     const episodeDocRef = await getEpisodeDocData(webtoonID, episodeID);
 
     const episodeDoc = await getDoc(episodeDocRef);
@@ -61,5 +82,22 @@ async function commnetCount(webtoonID,episodeID){
         console.log("Comment count updated successfully");
     } catch (error) {
         console.error("Error updating comment count: ", error);
+    }
+}
+
+async function deleteComment(webtoonID, episodeNumber, commentToDelete,episodeID,UID) {
+    const episodeRef = doc(db, "COMMENT", webtoonID, "Episodes", episodeNumber);
+    const episodeDocRef = await getEpisodeDocData(webtoonID, episodeID);
+    try {
+        await updateDoc(episodeRef, {
+            Comments: arrayRemove(commentToDelete)
+        });
+        await updateDoc(episodeDocRef, {
+            comment: increment(-1)
+        });
+
+        commentLoad(webtoonID, episodeNumber,episodeID,UID); // 댓글 목록 새로고침
+    } catch (error) {
+        console.error("Error deleting comment: ", error);
     }
 }
